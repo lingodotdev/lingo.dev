2c8fcc71290b9e26605c16bb626c1840032da912
NamesMT <dangquoctrung123@gmail.com>
2025-06-09 05:20:52 +0000

    refactor: don't assign vars in outer scope

--- packages/cli/src/cli/loaders/csv.ts
+++ packages/cli/src/cli/loaders/csv.ts
@@ -2,7 +2,7 @@ import { parse } from "csv-parse/sync";
 import { stringify } from "csv-stringify/sync";
 import _ from "lodash";
 import { ILoader } from "./_types";
-import { createLoader } from "./_utils";
+import { composeLoaders, createLoader } from "./_utils";
 
 /**
  * Tries to detect the key column name from a csvString.
@@ -15,35 +15,46 @@ export function detectKeyColumnName(csvString: string) {
   return firstColumn || "KEY";
 }
 
-export default function createCsvLoader(): ILoader<
-  string,
-  Record<string, string>
-> {
-  let inputParsed: Record<string, any>[];
-  let keyColumnName: string = "KEY";
+export default function createCsvLoader() {
+  return composeLoaders(_createCsvLoader(), createPullOutputCleaner())
+}
 
+type InternalTransferState = {
+  keyColumnName: string;
+  inputParsed: Record<string, any>[];
+  items: Record<string, string>;
+}
+
+function _createCsvLoader(): ILoader<
+  string,
+  InternalTransferState
+> {
   return createLoader({
     async pull(locale, input) {
-      inputParsed = parse(input, {
+      const keyColumnName = detectKeyColumnName(input.split('\n').find(l => l.length)!);
+      const inputParsed = parse(input, {
         columns: true,
         skip_empty_lines: true,
         relax_column_count_less: true,
       }) as Record<string, any>[];
 
-      keyColumnName = detectKeyColumnName(input.split('\n').find(l => l.length)!);
-
-      const result: Record<string, string> = {};
-
+      const items: Record<string, string> = {};
+      
+      // Assign keys that already have translation so AI doesn't re-generate it.
       _.forEach(inputParsed, (row) => {
         const key = row[keyColumnName];
         if (key && row[locale] && row[locale].trim() !== "") {
-          result[key] = row[locale];
+          items[key] = row[locale];
         }
       });
 
-      return result;
+      return {
+        inputParsed,
+        keyColumnName,
+        items
+      };
     },
-    async push(locale, data) {
+    async push(locale, {inputParsed, keyColumnName, items}) {
       const columns = inputParsed.length > 0 ? Object.keys(inputParsed[0]) : [keyColumnName, locale];
       if (!columns.includes(locale)) {
         columns.push(locale);
@@ -51,11 +62,11 @@ export default function createCsvLoader(): ILoader<
 
       const updatedRows = inputParsed.map((row) => ({
         ...row,
-        [locale]: data[row[keyColumnName]] || row[locale] || "",
+        [locale]: items[row[keyColumnName]] || row[locale] || "",
       }));
       const existingKeys = new Set(inputParsed.map((row) => row[keyColumnName]));
 
-      Object.entries(data).forEach(([key, value]) => {
+      Object.entries(items).forEach(([key, value]) => {
         if (!existingKeys.has(key)) {
           const newRow: Record<string, string> = {
             [keyColumnName]: key,
@@ -70,6 +81,23 @@ export default function createCsvLoader(): ILoader<
         header: true,
         columns,
       });
+    },
+  });
+}
+
+/**
+ * This is a simple extra loader that is used to clean the data written to lockfile
+ */
+function createPullOutputCleaner(): ILoader<
+  InternalTransferState,
+  Record<string, string>
+> {
+  return createLoader({
+    async pull(_locale, input) {
+      return input.items;
+    },
+    async push(_locale, data, _oI, _oL, pullInput) {
+      return { ...pullInput!, items:data };
     },
   });
 }
