c2daf0697d7df87ad296b328a83bf9f3cc88c6f8
maxprilutskiy <maks.prilutskiy@gmail.com>
2025-06-12 23:20:52 -0700

    chore: upd demo

--- demo/next-app/src/lingo/dictionary.js
+++ demo/next-app/src/lingo/dictionary.js
@@ -8,6 +8,7 @@ export default {
             ar: "قم بتعريب تطبيق React الخاص بك بجميع اللغات في دقائق.<element:br></element:br> توسع ليصل إلى الملايين منذ اليوم الأول.",
             de: "Lokalisieren Sie Ihre React-App in jeder Sprache innerhalb von Minuten.<element:br></element:br> Skalieren Sie vom ersten Tag an auf Millionen.",
             en: "Localiza tu aplicación React en todos los idiomas en minutos.<element:br></element:br> Escala a millones desde el primer día.",
+            es: "Localiza tu aplicación React en cualquier idioma en minutos.<element:br></element:br> Escala a millones desde el primer día.",
             fr: "Localisez votre application React dans toutes les langues en quelques minutes.<element:br></element:br> Adaptez-vous à des millions d'utilisateurs dès le premier jour.",
             ja: "数分でReactアプリをあらゆる言語にローカライズ。<element:br></element:br> 初日から数百万規模へスケール。",
             ko: "몇 분 안에 React 앱을 모든 언어로 현지화하세요.<element:br></element:br> 첫날부터 수백만 규모로 확장하세요.",
