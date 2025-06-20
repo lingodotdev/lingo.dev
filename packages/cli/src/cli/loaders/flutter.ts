b168786143dd68e47c07ae8c60debe6f48f0838b
Matej Lednicky <matej@lednicky.name>
2025-06-20 13:31:22 +0200

    fix(cli): only re-add metadata keys on push

--- packages/cli/src/cli/loaders/flutter.ts
+++ packages/cli/src/cli/loaders/flutter.ts
@@ -2,16 +2,27 @@ import _ from "lodash";
 import { ILoader } from "./_types";
 import { createLoader } from "./_utils";
 
-export default function createFlutterLoader(): ILoader<Record<string, any>, Record<string, any>> {
+export default function createFlutterLoader(): ILoader<
+  Record<string, any>,
+  Record<string, any>
+> {
   return createLoader({
     async pull(locale, input) {
       // skip all metadata (keys starting with @)
-      const result = _.pickBy(input, (value, key) => !key.startsWith("@"));
+      const result = _.pickBy(input, (value, key) => !_isMetadataKey(key));
       return result;
     },
     async push(locale, data, originalInput) {
-      const result = _.merge({}, originalInput, { "@@locale": locale }, data);
+      // find all metadata keys in originalInput
+      const metadata = _.pickBy(originalInput, (value, key) =>
+        _isMetadataKey(key),
+      );
+      const result = _.merge({}, metadata, { "@@locale": locale }, data);
       return result;
     },
   });
+}
+
+function _isMetadataKey(key: string) {
+  return key.startsWith("@");
 }
