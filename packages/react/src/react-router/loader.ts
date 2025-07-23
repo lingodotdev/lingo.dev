24e78deb32adab94c9bce690dae087d4eb01d451
David Turnbull <realdavidturnbull@gmail.com>
2025-07-23 13:42:16 +1000

    fix: fix loadDictionary for React Router

--- packages/react/src/react-router/loader.ts
+++ packages/react/src/react-router/loader.ts
@@ -6,32 +6,9 @@ export const loadDictionary = async (
   return null;
 };
 
-export async function loadDictionary_internal(
-  requestOrExplicitLocale: Request | string,
-  dictionaryLoader: Record<string, () => Promise<any>>,
-) {
-  const locale = parseLocaleCode(requestOrExplicitLocale);
-
-  // get dictionary loader for specific locale
-  const loader = dictionaryLoader[locale];
-
-  // locale is not available in the dictionary
-  if (!loader) {
-    // TODO: throw a clear error message
-    return null;
-  }
-
-  return loader().then((value) => value.default);
-}
-
-function parseLocaleCode(input: Request | string): string {
-  // if it's a string, assume it's a locale code
-  if (typeof input === "string") {
-    return input;
-  }
-
+function loadLocaleFromCookies(request: Request) {
   // it's a Request, so get the Cookie header
-  const cookieHeaderValue = input.headers.get("Cookie");
+  const cookieHeaderValue = request.headers.get("Cookie");
 
   // there's no Cookie header, so return default
   if (!cookieHeaderValue) {
@@ -39,8 +16,9 @@ function parseLocaleCode(input: Request | string): string {
   }
 
   // get the lingo-locale cookie
-  const cookies = cookieHeaderValue.split(";");
-  const cookie = cookies.find(isLingoLocaleCookie);
+  const cookie = cookieHeaderValue
+    .split(";")
+    .find((cookie) => cookie.trim().startsWith(`${LOCALE_COOKIE_NAME}=`));
 
   // there's no lingo-locale cookie, so return default
   if (!cookie) {
@@ -50,6 +28,24 @@ function parseLocaleCode(input: Request | string): string {
   return cookie;
 }
 
-function isLingoLocaleCookie(cookie: string) {
-  return cookie.trim().startsWith(`${LOCALE_COOKIE_NAME}=`);
+export async function loadDictionary_internal(
+  requestOrExplicitLocale: Request | string,
+  dictionaryLoader: Record<string, () => Promise<any>>,
+) {
+  // gets the locale (falls back to "en")
+  const locale =
+    typeof requestOrExplicitLocale === "string"
+      ? requestOrExplicitLocale
+      : loadLocaleFromCookies(requestOrExplicitLocale);
+
+  // get dictionary loader for the locale
+  const loader = dictionaryLoader[locale];
+
+  // locale is not available in the dictionary
+  if (!loader) {
+    // TODO: throw a clear error message
+    return null;
+  }
+
+  return loader().then((value) => value.default);
 }
