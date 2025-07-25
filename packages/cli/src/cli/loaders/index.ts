43917b520eae2533dd0a5d6576717c46c4d60891
VAIBHAVSING <vpatil5212@gmail.com>
2025-07-25 15:56:17 +0530

    feat(cli): fix typescript ignoredKeys for export default and exports

--- packages/cli/src/cli/loaders/index.ts
+++ packages/cli/src/cli/loaders/index.ts
@@ -300,10 +300,10 @@ export default function createBucketLoader(
         createTypescriptLoader(),
         createFlatLoader(),
         createEnsureKeyOrderLoader(),
-        createSyncLoader(),
         createLockedKeysLoader(lockedKeys || []),
+        createSyncLoader(),
         createIgnoredKeysLoader(ignoredKeys || []),
-        createUnlocalizableLoader(options.returnUnlocalizedKeys),
+        createUnlocalizableLoader(options.returnUnlocalizedKeys, ignoredKeys),
       );
     case "txt":
       return composeLoaders(
