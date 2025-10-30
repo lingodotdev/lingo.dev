77a2408339e1c8aab593cc93591eb55e82bc48f7
Aditya-Prakash14 <ap312038@gmail.com>
2025-10-30 11:31:05 +0530

    fix: make theme toggle visible on mobile devices

--- demo/vite-project/src/App.tsx
+++ demo/vite-project/src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import { LingoDotDev } from "./lingo-dot-dev";
 import "./App.css";
 import { TestComponent } from "./components/test";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 import { LocaleSwitcher } from "lingo.dev/react/client";
 
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <>
+      <ThemeToggle />
       <div className="logo-container">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
