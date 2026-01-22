1d7cba0c6454d56ea384f199483053cfd147c264
sahin Islam <sahinislam878@gmail.com>
2026-01-22 16:03:26 +0530

    Create Repository

--- community/lingo-notify/src/main/java/com/lingonotify/repository/NotificationRepository.java
+++ community/lingo-notify/src/main/java/com/lingonotify/repository/NotificationRepository.java
@@ -1,4 +1,8 @@
 package com.lingonotify.repository;
 
-public class NotificationRepository {
+import com.lingonotify.model.NotificationLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository
+  extends JpaRepository<NotificationLog, Long> {
 }
