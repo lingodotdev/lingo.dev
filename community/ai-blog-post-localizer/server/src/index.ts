b4cbcb3550da7adab2ed1483b031350ee8aa07f5
VaibhavOrbit <vaibhav.readme@gmail.com>
2026-01-22 15:23:05 +0000

    create a /content endpoint

---
+++ community/ai-blog-post-localizer/server/src/index.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+import express from "express"; 
+import dotenv from 'dotenv';
+
+
+dotenv.config(); 
+const app = express(); 
+app.use(express.json()); 
+const PORT = process.env.PORT; 
+
+
+const I18N_PATH = path.join(process.cwd(), "i18n", "en.json"); 
+
+
+app.post("/content",async (req,res)=>{
+    try {
+    const content = req.body(); 
+
+    if(typeof content !== 'object') {
+        return res.status(400).json({
+            error: "Invaild Content"
+        }); 
+    }
+
+    fs.writeFileSync(
+        I18N_PATH,
+        JSON.stringify(content, null, 2),
+        "utf-8"
+    )
+        res.json({message: "en.json updated successfully"})
+    } 
+
+    catch(e) { 
+        res.status(500).json({
+            error: "Failed to write content"
+        })
+    }
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`{Server Started at Port: ${PORT}`)
+})
