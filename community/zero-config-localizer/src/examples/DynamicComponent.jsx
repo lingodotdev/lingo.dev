59de6797eac7f8627ae5ad11945f939cc0d65bfa
Apoorva gupta <apoorvagupta@Apoorvas-MacBook-Air-2.local>
2026-01-25 13:04:03 +0530

    docs: add JSDoc comments to  DynamicComponent

--- community/zero-config-localizer/src/examples/DynamicComponent.jsx
+++ community/zero-config-localizer/src/examples/DynamicComponent.jsx
@@ -9,17 +9,27 @@
  */
 
 import { useLingoLocale } from 'lingo.dev/react/client';
-  
-  export default function DynamicComponent({ userName, messageCount, createdAt }) {
-    const locale = useLingoLocale();
-    const createdOn = new Date(createdAt).toLocaleDateString(locale ?? undefined);
-    return (
-      <div>
-        <h2>Hello, {userName}!</h2>
-        <p>You have {messageCount} new messages</p>
-        <p>Your account was created on {createdOn}</p>
-      </div>
-    );
+
+export default function DynamicComponent({ userName, messageCount, createdAt }) {
+  const locale = useLingoLocale();
+
+  const isDateOnlyISO = /^\d{4}-\d{2}-\d{2}$/.test(createdAt);
+
+  let createdOnDate;
+  if (isDateOnlyISO) {
+    const [year, month, day] = createdAt.split('-').map(Number);
+    createdOnDate = new Date(year, month - 1, day); // local date
+  } else {
+    createdOnDate = new Date(createdAt);
   }
-  
-  
+
+  const createdOn = createdOnDate.toLocaleDateString(locale ?? undefined);
+
+  return (
+    <div>
+      <h2>Hello, {userName}!</h2>
+      <p>You have {messageCount} new messages</p>
+      <p>Your account was created on {createdOn}</p>
+    </div>
+  );
+}
