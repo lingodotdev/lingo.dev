0764fcefde7ab767834401f539f0f5880ae5d4a9
Artur <artur.cherkanov@gmail.com>
2026-08-19 13:49:48 +0200

    feat(compiler): honour the configured timeout in pluralization too

--- packages/new-compiler/src/translators/translation-service.ts
+++ packages/new-compiler/src/translators/translation-service.ts
@@ -109,6 +109,7 @@ export class TranslationService {
             {
               ...this.config.pluralization,
               sourceLocale: this.config.sourceLocale,
+              aiTimeout: config.aiTimeout,
             },
             this.logger,
           );
